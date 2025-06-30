33c64d751105a3c18358671572fb3ffed23db279
Mike-qian <mike1994976451@outlook.com>
2025-07-01 00:17:53 +0800

    Update diy5.sh

--- diy5.sh
+++ diy5.sh
@@ -1,2 +1,2 @@
 sed -i '$a src-git xuanranran https://github.com/xuanranran/openwrt-package.git' feeds.conf.default
-sed -i '$a src-git kiddin9 https://github.com/kiddin9/kwrt-packages.git' feeds.conf.default
+# sed -i '$a src-git kiddin9 https://github.com/kiddin9/kwrt-packages.git' feeds.conf.default
