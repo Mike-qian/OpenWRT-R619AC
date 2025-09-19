2fefffd2c83aeb60b25bf37f57a55b3d8852454a
Mike-qian <mike1994976451@outlook.com>
2025-09-19 20:20:39 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -23,3 +23,5 @@
 
 wget -P ./target/linux/ipq40xx/base-files/etc/hotplug.d/block/ https://raw.githubusercontent.com/Mike-qian/dns2tcp/main/20-udisk-mount
 chmod 0755 target/linux/ipq40xx/base-files/etc/hotplug.d/block/20-udisk-mount
+sed -i 's/option check_signature 1/option check_signature 0/' package/system/opkg/files/opkg-smime.conf
+sed -i '$a src/gz openwrt_kiddin9 https://dl.openwrt.ai/latest/packages/arm_cortex-a7_neon-vfpv4/kiddin9' package/system/opkg/files/customfeeds.conf
